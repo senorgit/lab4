6216f483035584421529f25ac1dcdf3a1a6d0ec1
Andrew Pham <srdengr@gmail.com>
2020-03-04 22:07:51 -0800

    updated lab4 code and added lab report

--- my_switch/my_switch.c
+++ my_switch/my_switch.c
@@ -1,21 +1,10 @@
 #include <stdint.h>
 #include "inc/tm4c123gh6pm.h"
 
-#define 	BLUE_MASK 		0x04
-//*****************************************************************************
-//
-//!
-//! A very simple example that interfaces with the blud LED (PF2) and SW2 (PF0) 
-//! using direct register access. When SW2 is pressed, the LED is turned on. When 
-//! SW2 is released, the LED is turned off. 
-//
-//*****************************************************************************
+#define 	MASK 		0x04  //variable MASK = LED pin position. 0x02 = RED, 0x04=BLUE and 0x08 = GREEN
 
-
-void
-PortFunctionInit(void)
+void PortFunctionInit(void)
 {
-
 		volatile uint32_t ui32Loop;   
 	
 		// Enable the clock of the GPIO port that is used for the on-board LED and switch.
@@ -30,24 +19,21 @@ PortFunctionInit(void)
 		GPIO_PORTF_LOCK_R = 0x4C4F434B;   
 		GPIO_PORTF_CR_R |= 0x01;           // allow changes to PF0
 
-    // Set the direction of PF1 (red LED) as output
-    GPIO_PORTF_DIR_R |= 0x02;
+    // Set the direction of LED as output
+    GPIO_PORTF_DIR_R |= MASK;
 	
 		// Set the direction of PF0 (SW2) as input by clearing the bit
     GPIO_PORTF_DIR_R &= ~0x01;
 	
-    // Enable both PF1 and PF0 for digital function.
-    GPIO_PORTF_DEN_R |= 0x03;
+    // Enable both LED and PF0 for digital function.
+    GPIO_PORTF_DEN_R |= MASK + 0x01;
 	
 		//Enable pull-up on PF0
 		GPIO_PORTF_PUR_R |= 0x01; 
-
 }
 
-
 int main(void)
-{
-	
+{	
 		//initialize the GPIO ports	
 		PortFunctionInit();
 	
@@ -55,17 +41,16 @@ int main(void)
     // Loop forever.
     //
     while(1)
-    {
-
+			{
         if((GPIO_PORTF_DATA_R&0x01)==0x01) //SW2 is NOT pressed
 				{
 						// Turn on the LED.
-						GPIO_PORTF_DATA_R |= 0x02;
+						GPIO_PORTF_DATA_R |= MASK;
 				}
 				else
 				{
 						// Turn off the LED.
-						GPIO_PORTF_DATA_R &= ~0x02;
+						GPIO_PORTF_DATA_R &= ~MASK;
 				}
     }
 }
--- my_toggle/my_toggle.c
+++ my_toggle/my_toggle.c
@@ -3,61 +3,39 @@
 #include "inc/tm4c123gh6pm.h"
 #include "driverlib/sysctl.h"
 
-#define 	RED_MASK 		0x02
-#define 	GREEN_MASK		0x08
-//*****************************************************************************
-//
-//!
-//! A very simple example that toggles the on-board red LED using direct register
-//! access.
-//
-//*****************************************************************************
+#define 	MASK		0x08 //Define MASK as LED variable.  In this case it's 0x08 or GREEn LED
 
-
-void
-PortFunctionInit(void)
+void PortFunctionInit(void)
 {
-//
-		volatile uint32_t ui32Loop;   
-	// Enable the GPIO port that is used for the on-board LED.
-    //
+  	volatile uint32_t ui32Loop;   
+	  // Enable the GPIO port that is used for the on-board LED.    
     SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
-
-    //
-    // Do a dummy read to insert a few cycles after enabling the peripheral.
-    //
+    
+    // Do a dummy read to insert a few cycles after enabling the peripheral.    
     ui32Loop = SYSCTL_RCGC2_R;
 
-    //
     // Enable the GPIO pin for the red LED (PF1).  Set the direction as output, and
-    // enable the GPIO pin for digital function.
-    //
-    GPIO_PORTF_DIR_R |= 0x08;
-    GPIO_PORTF_DEN_R |= 0x08;
-
+    // enable the GPIO pin for digital function.    
+    GPIO_PORTF_DIR_R |= MASK;
+    GPIO_PORTF_DEN_R |= MASK;
 }
 
 
 int main(void)
-{
-	
+{	
 		//initialize the GPIO ports	
 		PortFunctionInit();
 	
     // Turn on the LED.
-    GPIO_PORTF_DATA_R |= 0x08;
-
-    
-    //
-    // Loop forever.
-    //
+    GPIO_PORTF_DATA_R |= MASK;
+	
+    // Loop forever.    
     while(1)
     {
         // Delay for a bit.
 				SysCtlDelay(10000000);	
 
         // Toggle the LED.
-        GPIO_PORTF_DATA_R ^=GREEN_MASK;
-				//GPIO_PORTF_DATA_R ^= 0x02; //either use the RED_MASK variable or could give it an immediate value
+        GPIO_PORTF_DATA_R ^= MASK;				
     }
 }
